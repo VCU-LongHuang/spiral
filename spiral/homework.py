870d68cc4f8540462416d4c3046589cce1241346
VCU-LongHuang <78453837+VCU-LongHuang@users.noreply.github.com>
2021-04-09 22:13:32 -0400

    ok

--- spiral/homework.py
+++ spiral/homework.py
@@ -1,4 +1,9 @@
 def spiralize(number):
     return_value = 1
+    n = (number - 1) // 2
+    x = (2 * n * ((8 * n * n) + (15 * n) + 16)) / 3
 
+    return_value = x
     return return_value
+
+print(spiralize(501))
