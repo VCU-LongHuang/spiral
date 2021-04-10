d3e3cfe72a149e405283e6b8ffefa7714e579ae3
VCU-LongHuang <78453837+VCU-LongHuang@users.noreply.github.com>
2021-04-09 22:48:18 -0400

    ok.......

--- spiral/homework.py
+++ spiral/homework.py
@@ -1,7 +1,9 @@
 def spiralize(number):
     return_value = 1
     diagonal = (number - 1) // 2
-    return_value = (diagonal * (8 * diagonal * diagonal + 13 + diagonal * 15) * 2 + 3) / 3
+    return_value = (
+        diagonal * (8 * diagonal * diagonal + 13 + diagonal * 15) * 2 + 3
+    ) / 3
 
     return return_value
 
